e9a21383ceaaead21da2cac879b04bea8ccd0627
Samuel White <53321354+SammyWhite000@users.noreply.github.com>
2022-02-14 08:38:42 -0700

    Update uploadData.java

--- uploadData.java
+++ uploadData.java
@@ -8,30 +8,58 @@
 */
 import java.io.*;
 class uploadData{
-    public static void main(String[] args) {
-      //Presume This "fetches the file"
+    public static void main(String[] args){
+      System.out.println("It is working");
+      try{
+         //First start reading the data file and create a temp file
+         BufferedReader reader = new BufferedReader(new FileReader("data.txt"));
 
-      //One of the esiest ways to use file I/O in Java
-      //The reason this is freaking out is becuase it is saying that this code,
-      //CAN thow an exception but we are not dong anyting with it
-      try{
-         BufferedReader reader = new BufferedReader(new FileReader("random.txt"));
-      }catch(IOException e){
-         e.printStackTrace();
-      }
-      try{
-         //First write to a file the number of lines 
-         int flag =0;
-         BufferedWriter writer = new BufferedWriter(new FileWriter("data.txt"));
-         //flag == 0 ? writer.write("Reee") : writer.write("\nfuck");
-         if(flag == 0){ writer.write("Fuck");}
-         else{
-            writer.write("\nReee");
+         File tempFile = File.createTempFile("tempFile", null);
+         String loc = tempFile.getAbsolutePath();
+         //System.out.println(loc);
+
+         //Start writing to the temp file
+         BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+         //Start writing to the file
+         //Check if we have looped through once
+         int flag = 0;
+         String lineByLine;
+         //Loop through file
+         while((lineByLine = reader.readLine()) != null){
+            if(flag == 0){
+               flag = 1;
+               int increase = Integer.parseInt(lineByLine) + 1;
+               writer.write(String.valueOf(increase));
+               writer.write("\n");
+            }
+            else{
+               writer.write(lineByLine);      
+               writer.write("\n");
+            }
          }
+         reader.close();
          writer.close();
+
+         //Clear all the contents of data
+         /*
+         PrintWriter clearData = new PrintWriter("data.txt");
+         clearData.print("");
+         clearData.close();
+         */
+
+         //Once I write to temp file, close and open buffer writer and reader
+         //Reader of temp file
+         //Writer of data.txt
+         BufferedReader tempReader = new BufferedReader(new FileReader("output.txt"));
+         BufferedWriter dataWriter = new BufferedWriter(new FileWriter("data.txt"));
+         while((lineByLine = tempReader.readLine()) != null){
+            dataWriter.write("data.txt");
+            dataWriter.write("\n");
+         }
+         tempReader.close();
+         dataWriter.close();
       }catch(IOException e){
          e.printStackTrace();
       }
-
     }
 }
